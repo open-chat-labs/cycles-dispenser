d57dc3de2cc40d45b8e24ff509f511cde6120082
Hamish Peebles <hamishpeebles@gmail.com>
2022-10-21 15:53:52 +0100

    Add `InitArgs` to candid file

--- canister/api/src/lifecycle/init.rs
+++ canister/api/src/lifecycle/init.rs
@@ -1,11 +1,11 @@
 use candid::{CandidType, Principal};
 use serde::{Deserialize, Serialize};
-use types::{Cycles, Milliseconds};
+use types::{CanisterId, Cycles, Milliseconds};
 
 #[derive(CandidType, Serialize, Deserialize, Debug)]
 pub struct Args {
     pub admins: Vec<Principal>,
-    pub canisters: Vec<Principal>,
+    pub canisters: Vec<CanisterId>,
     pub top_up_amount: Cycles,
     pub min_interval: Milliseconds,
     pub min_cycles_balance: Cycles,
