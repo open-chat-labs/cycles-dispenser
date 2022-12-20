ede4457d534dd061b016dbfe317bf5c5c21baa9e
Hamish Peebles <hamishpeebles@gmail.com>
2022-12-20 18:50:21 +0000

    Handle `add_canister` in `inspect_message`

--- canister/impl/src/lifecycle/inspect_message.rs
+++ canister/impl/src/lifecycle/inspect_message.rs
@@ -1,6 +1,24 @@
+use crate::{read_state, State};
 use ic_cdk_macros::inspect_message;
 
 #[inspect_message]
 fn inspect_message() {
-    // By not calling `ic_cdk::api::call::accept_message()` we reject all ingress messages
+    let method_name = ic_cdk::api::call::method_name();
+
+    // 'inspect_message' only applies to ingress messages so calls to c2c methods should be rejected
+    let is_c2c_method = method_name.starts_with("c2c");
+    if is_c2c_method {
+        return;
+    }
+
+    if read_state(|state| is_valid(&method_name, state)) {
+        ic_cdk::api::call::accept_message();
+    }
+}
+
+fn is_valid(method_name: &str, state: &State) -> bool {
+    match method_name {
+        "add_canister" => state.is_caller_admin(),
+        _ => false,
+    }
 }
