61d2ecec5e143397b5f00f5dc941f41519bc2918
Matt Grogan <megrogan@gmail.com>
2023-02-15 12:13:09 +0000

    set_governance_principals (#3)

--- canister/impl/src/lifecycle/inspect_message.rs
+++ canister/impl/src/lifecycle/inspect_message.rs
@@ -18,7 +18,9 @@ fn inspect_message() {
 
 fn is_valid(method_name: &str, state: &State) -> bool {
     match method_name {
-        "add_canister" | "update_config" => state.is_caller_admin(),
+        "add_canister" | "set_governance_principals" | "update_config" => {
+            state.is_caller_governance_principal()
+        }
         _ => false,
     }
 }
